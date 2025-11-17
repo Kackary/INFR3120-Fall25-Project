b6bbc952bea0be8ccd4c3f879199d1e3957bc1ae
pboi-coder <71351814+pboi-coder@users.noreply.github.com>
2025-11-16 23:53:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -102,3 +102,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
